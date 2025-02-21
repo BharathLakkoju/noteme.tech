963f0d3393b8e651ba379dc68614a8d7142ba15f
bharathlakkoju <lbh.lbharath@gmail.com>
2025-02-21 15:05:16 +0530

    new dialog box fix

--- src/components/NewNoteDialog.tsx
+++ src/components/NewNoteDialog.tsx
@@ -27,7 +27,7 @@ export function NewNoteDialog({ isOpen, onClose }: NewNoteDialogProps) {
 
   return (
     <div
-      className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center p-4"
+      className="fixed w-screen h-screen inset-0 bg-black/50 z-50 flex items-center justify-center p-4"
       onClick={(e) => {
         if (e.target === e.currentTarget) {
           onClose();
